894d3dd8c8863de702a05dc06d79b85fa05117a1
Gaurav Rajput <gauravrajput@Gauravs-MacBook-Air.local>
2025-08-30 21:48:46 +0530

    package name change in android com.worldener

--- android/app/src/main/java/com/worldener/MainActivity.kt
+++ android/app/src/main/java/com/worldener/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.wapp
+package com.worldener
 import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/worldener/MainApplication.kt
+++ android/app/src/main/java/com/worldener/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.wapp
+package com.worldener
 
 import android.app.Application
 import com.facebook.react.PackageList
